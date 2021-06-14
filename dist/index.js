9e3e46d9a59945ab58431961fefeb8eaf27ac345
Fumihiro.Saito <Fumihiro.Saito@access-company.com>
2021-06-14 20:51:25 +0900

    Update react-leaflet.

--- dist/index.js
+++ dist/index.js
@@ -59,6 +59,19 @@ function _setPrototypeOf(o, p) {
   return _setPrototypeOf(o, p);
 }
 
+function _isNativeReflectConstruct() {
+  if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+  if (Reflect.construct.sham) return false;
+  if (typeof Proxy === "function") return true;
+
+  try {
+    Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {}));
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
 function _assertThisInitialized(self) {
   if (self === void 0) {
     throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
@@ -75,15 +88,34 @@ function _possibleConstructorReturn(self, call) {
   return _assertThisInitialized(self);
 }
 
-var FullscreenControl =
-/*#__PURE__*/
-function (_MapControl) {
+function _createSuper(Derived) {
+  var hasNativeReflectConstruct = _isNativeReflectConstruct();
+
+  return function _createSuperInternal() {
+    var Super = _getPrototypeOf(Derived),
+        result;
+
+    if (hasNativeReflectConstruct) {
+      var NewTarget = _getPrototypeOf(this).constructor;
+
+      result = Reflect.construct(Super, arguments, NewTarget);
+    } else {
+      result = Super.apply(this, arguments);
+    }
+
+    return _possibleConstructorReturn(this, result);
+  };
+}
+
+var FullscreenControl = /*#__PURE__*/function (_MapControl) {
   _inherits(FullscreenControl, _MapControl);
+
+  var _super = _createSuper(FullscreenControl);
 
   function FullscreenControl() {
     _classCallCheck(this, FullscreenControl);
 
-    return _possibleConstructorReturn(this, _getPrototypeOf(FullscreenControl).apply(this, arguments));
+    return _super.apply(this, arguments);
   }
 
   _createClass(FullscreenControl, [{
