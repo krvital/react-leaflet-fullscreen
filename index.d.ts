c10234d0eb42e0676ecc3d0a9b31d781f3ee8941
Vitaly Kravtsov <krvital@fliptalk.team>
2023-08-07 21:55:16 +0600

    update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,22 +1,9 @@
-import L from 'leaflet';
-
-declare module 'leaflet' {
-  namespace control {
-    function fullscreen(options?: FullscreenOptions): FullscreenControl;
-  }
-
-  interface FullscreenOptions {
+interface FullscreenOptions {
     position?: 'topleft' | 'topright' | 'bottomleft' | 'bottomright';
     title?: string;
     content?: string;
     forceSeparateButton?: boolean;
     forcePseudoFullscreen?: boolean;
     fullscreenElement?: false | HTMLElement;
-  }
-
-  interface FullscreenControl {
-    addTo(map: Map): FullscreenControl;
-    remove(): void;
-    link: HTMLAnchorElement;
-  }
 }
+export declare function FullscreenControl(props: FullscreenOptions): null;
